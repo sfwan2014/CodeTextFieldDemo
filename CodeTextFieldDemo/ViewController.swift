9396a4b332fc74e0587a2821d0a058e3d14d1dc4
Fred <854347546@qq.com>
2020-03-30 15:46:56 +0800

    Initial Commit

---
+++ CodeTextFieldDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeTextFieldDemo
+//
+//  Created by tezwez on 2020/3/30.
+//  Copyright © 2020 tezwez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
